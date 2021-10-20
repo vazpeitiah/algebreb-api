8a322916ca81031f37eaed0144ce338376e919e8
VladimirAh <vazpeitiah@gmail.com>
2021-10-20 11:36:14 -0500

    add success status on json responses

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,30 +7,42 @@ exports.signin = async (req, res, next) => {
     const { username, email, password } = req.body
 
     if (!username || !password) {
-        return res.json({ message: 'Please provide an username and password' }) // make middleware to handdle error
+        return res.json({ success: false, message: 'Please provide an username and password' }) // make middleware to handdle error
     }
 
     try {
         const user = await User.findOne({ username })
 
+        
+         
         if (!user) {
-            return res.json({ message: 'Invalid username' })
+            return res.json({ success: false,  message: 'Invalid username' })
         }
 
         const isMatch = await user.isValidPassword(password)
 
         if (isMatch == false) {
-            return res.json({ message: 'Invalid password' })
+            return res.json({ success: false,  message: 'Invalid password' })
         }
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: 60 * 60 * 24 // 24 horas
         })
 
-        return res.status(200).json({ succes: true, token })
+        const usrData = {
+            name: user.name,
+            username,
+            email: user.email,
+            token
+        }
+
+        const foundRoles = await Role.find({_id: {$in: user.roles}})
+        usrData.roles = foundRoles.map(role => role.name)
+
+        return res.status(200).json({ success: true, user: usrData })
 
     } catch (err) {
-        next(err)
+        return res.json({success: false, message:err.messasge})
     }
 }
 // Registrar cuenta de usuario nueva, devuelve un token valido
@@ -38,27 +50,38 @@ exports.signup = async (req, res, next) => {
     const {name, username, password, email, roles} = req.body
 
     if (!username || !password || !email || !roles) {
-        return res.json({ message: 'Please provide an username, password, email and the role of the user' }) // make middleware for handdle error
+        return res.json({success: false, message: 'Please provide an username, password, email and the role of the user' }) // make middleware for handdle error
     }
-
-    const newUser = {
-        name, username, password, email
-    }
-
-    const foundRoles = await Role.find({name: {$in: roles}})
-    newUser.roles = foundRoles.map(role => role._id)
 
     try {
+
+        const newUser = {
+            name, username, password, email
+        }
+    
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+
         const user = await User.create(newUser) // Arreglar para ver nombre de usuario ya existente
         
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: 60 * 60 * 24 // 24 horas
         })
 
-        return res.json({ status: "success", token })
+        const usrData = {
+            name: user.name,
+            username,
+            email: user.email,
+            token
+        }
+
+        const froles = await Role.find({_id: {$in: user.roles}})
+        usrData.roles = froles.map(role => role.name)
+
+        return res.json({ success: true, user:usrData})
 
     } catch (err) {
-        return res.json({ status: "error", message: err.message})
+        return res.json({success: false, message: err.message})
     }
 }
 
@@ -66,18 +89,18 @@ exports.signup = async (req, res, next) => {
 exports.verifyToken = async (req, res, next) => {
     const { token } = req.body;
     if (!token) {
-        return res.json({ message: "Provide a token" })
+        return res.json({success: false, message: "Provide a token" })
     }
     try {
         const verify = jwt.verify(token, process.env.JWT_SECRET);
         if (!verify) {
-            return res.json({ status: "fail" })
+            return res.json({ success: false, meesage: "Invalid token"})
         }
 
-        return res.json({ status: "success" })
+        return res.json({ success: true, message: token })
     } catch (err) {
 
-        return res.json({ status: "fail", message: err.message })
+        return res.json({ success: false, message: err.message })
         //next(err)
     }
 }
