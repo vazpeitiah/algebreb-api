4d4bbdc988406d2837239dd8f279f66887fb016d
VladimirAh <vazpeitiah@gmail.com>
2021-12-10 16:50:36 -0600

    Actualizando modulo de hojas

--- src/controllers/sheets.controller.js
+++ src/controllers/sheets.controller.js
@@ -1,63 +1,102 @@
-const sheetsController = {};
-
 const Sheet = require('../models/Sheet')
 
-sheetsController.getSheets = async (req, res) => {
-    const sheets = await Sheet.find()
-    res.json(sheets)
-}
+const sheetsController = {};
 
-sheetsController.getSheet = async (req, res) => {
-    const sheet = await Sheet.findById(req.params.sheetId);
-    res.json(sheet);
+sheetsController.getSheets = async (req, res) => {
+    try {
+        const sheets = await Sheet.find({ hidden:false })
+        return res.json({ success: true, sheets })
+    } catch (err) {
+        return res.json({ success:false, message: err.message })
+    }
 }
 
 sheetsController.getSheetsByUser = async (req, res) => {
-    const sheets = await Sheet.find({user: req.params.userId}).sort({date: -1})
-    res.json(sheets)
+    try {
+        const sheets = await Sheet.find({user: req.params.userId, hidden:false}, '-params -exercises -solutionsType -user').sort({date: -1})
+        return res.json({ success: true, sheets})
+    } catch (err) {
+        return res.json({ success: false, message: err.message })
+    }
+}
+
+sheetsController.getSheet = async (req, res) => {
+    try {
+        const sheet = await Sheet.findById(req.params.sheetId)
+        return res.json({ success: true, sheet })
+    } catch (err) {
+        return res.json({ success: false, message: err.message })
+    }
 }
 
 sheetsController.createSheet = async (req, res) => {
     const {description, date, type, user, exercises, params} = req.body
 
-    let body = {
-        description,
-        date,
-        type,
-        user,
-        solutionsType: 'oculta',
-        hidden: false
+    if(!description || !type || !user) {
+        return res.json({ 
+            success: false,
+            message: 'Provea los parametros description, type y user'
+        })
     }
 
-    if(exercises  && params) {
-        body = {...body, exercises, params, date: Date.now(), hidden: true}
+    try {
+        let body = {
+            description,
+            date,
+            type,
+            user,
+            solutionsType: 'oculta',
+            hidden: false
+        }
+    
+        if(exercises  && params) {
+            body = {...body, exercises, params, date: Date.now(), hidden: true}
+        }
+    
+        const sheet = await Sheet.create(new Sheet(body));
+        return res.json({ success: true, sheet })
+    } catch (err) {
+        return res.json({ success:false, message: err.message })
     }
-
-    const sheet = await Sheet.create(new Sheet(body));
-    res.json(sheet)
 }
 
 sheetsController.updateSheet = async (req, res) => {
     const {description, type, exercises, solutionsType, params} = req.body;
 
-    const updSheet = {
-        description,
-        date: Date.now(),
-        type,
-        solutionsType,
-        params
+    if(!description || !type || !solutionsType) {
+        return res.json({ 
+            success: false,
+            message: 'Provea los parametros description, type y solutionsType'
+        })
     }
 
-    const sheet = await Sheet.findByIdAndUpdate(req.params.sheetId, updSheet, {new: true});
-    sheet.exercises = exercises;
-    sheet.markModified('exercises')
-    sheet.save()
-    res.json(sheet)
+    try {
+        const updSheet = {
+            description,
+            date: Date.now(),
+            type,
+            solutionsType,
+            params
+        }
+    
+        const sheet = await Sheet.findByIdAndUpdate(req.params.sheetId, updSheet, {new: true});
+        sheet.exercises = exercises;
+        sheet.markModified('exercises')
+        sheet.save()
+
+        return res.json({ success: true, sheet })
+    } catch (err) {
+        return res.json({ success: false, message: err.message })
+    }
 }
 
 sheetsController.deleteSheet = async (req, res) => {
-    await Sheet.findByIdAndDelete(req.params.sheetId);
-    res.json({message: "Sheet deleted"});
+    try {
+        const sheet = await Sheet.findByIdAndDelete(req.params.sheetId);
+        return res.json({ success: true, sheet });
+    } catch (err) {
+        return res.json({ success: false, message: err.message })
+    }
 }
 
 module.exports = sheetsController;
--- src/routes/sheets.route.js
+++ src/routes/sheets.route.js
@@ -4,13 +4,11 @@ const router = Router()
 const sheetsController = require('../controllers/sheets.controller')
 const {verifyToken} = require('../controllers/verfify.controller')
 
-router.route('/').get(verifyToken, sheetsController.getSheets)
-router.route('/byuser/:userId').get(verifyToken, sheetsController.getSheetsByUser)
-router.route('/:sheetId').get(verifyToken, sheetsController.getSheet)
-
-router.route('/').post(verifyToken, sheetsController.createSheet)
-router.route('/:sheetId').put(verifyToken, sheetsController.updateSheet)
-router.route('/:sheetId').delete(verifyToken, sheetsController.deleteSheet)
-
+router.get('/byuser/:userId', verifyToken, sheetsController.getSheetsByUser)
+router.get('/', verifyToken, sheetsController.getSheets)
+router.post('/', verifyToken, sheetsController.createSheet)
+router.get('/:sheetId', verifyToken, sheetsController.getSheet)
+router.put('/:sheetId', verifyToken, sheetsController.updateSheet)
+router.delete('/:sheetId', verifyToken, sheetsController.deleteSheet)
 
 module.exports = router
